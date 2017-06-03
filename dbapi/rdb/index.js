ce9ccc135c8cd7642d19eb2fe2e318ff0f181b57
Leo Liu <leoliuasia@163.com>
2017-06-03 16:59:24 +0800

    added superadmin when db is initializing.

--- dbapi/rdb/index.js
+++ dbapi/rdb/index.js
@@ -34,6 +34,19 @@ r.connect({db: rconf.db, host: rconf.host, port: rconf.port}, (err, conn) => {
             r.tableCreate(table).run(conn, (err, result) => {
               if (err) throw err;
               createIndex(table, indexes[table]);// create index.
+              if (table == 'users') {
+                r.table(table).insert({ // 添加超级用户
+                  usname: 'admin',
+                  pwd: 'e10adc3949ba59abbe56e057f20f883e',
+                  nickname: '亮哥哥',
+                  role: 1,
+                  regtime: "2017-05-23 10:20:00",
+                  lasttime: "2017-05-23 10:20:00",
+                  lastip: "127.0.0.1",
+                  status: 0
+                }).run(exports.conn)
+                  .then((result)=>{}).catch((err)=>{});
+              }
             });
           } else {
             createIndex(table, indexes[table]);// create index.
